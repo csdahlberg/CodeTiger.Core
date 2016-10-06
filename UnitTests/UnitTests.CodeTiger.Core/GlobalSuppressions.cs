50472b206317a7b24696ccc77847363059d85cf7
Chris Dahlberg <csdahlberg@gmail.com>
2016-10-05 21:17:26 -0500

    Suppressed CT1702 (which requires pascal casing of type names) in UnitTests.CodeTiger.Core.

---
+++ UnitTests/UnitTests.CodeTiger.Core/GlobalSuppressions.cs
@@ -0,0 +1,4 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("CodeTiger.Naming", "CT1702:Type names should use pascal casing.",
+    Justification = "Unit test classes do not need to use pascal casing.")]
