4d5b84971c1ce3b8296f0e479c606e70c68ef0ac
Chris Dahlberg <csdahlberg@gmail.com>
2020-02-10 20:19:23 -0600

    Updated CodeTiger.CodeAnalysis to 1.0.1-beta02.

--- UnitTests/UnitTests.CodeTiger.Core/GlobalSuppressions.cs
+++ UnitTests/UnitTests.CodeTiger.Core/GlobalSuppressions.cs
@@ -6,3 +6,5 @@
     Justification = "Unit test classes do not need to use pascal casing.")]
 [assembly: SuppressMessage("Design", "CA1034:Nested types should not be visible",
     Justification = "Unit test classes are allowed to be nested.")]
+[assembly: SuppressMessage("CodeTiger.Naming", "CT1727:Methods returning a Task should be suffixed with 'Async'.",
+    Justification = "Unit test method names do not need to have an Async suffix.")]
