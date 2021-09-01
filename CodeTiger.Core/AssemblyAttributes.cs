f29382369f5b3132537ba7bdc13b5a1067daf753
Chris Dahlberg <csdahlberg@gmail.com>
2021-08-31 20:47:08 -0500

    Added CLSCompliant assembly attributes.

--- CodeTiger.Core/AssemblyAttributes.cs
+++ CodeTiger.Core/AssemblyAttributes.cs
@@ -1,5 +1,7 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
 
+[assembly: CLSCompliant(true)]
 [assembly: InternalsVisibleTo("UnitTests.CodeTiger.Core, PublicKey=00240000048000009400000006020000002400005253413"
     + "10004000001000100f1abc75191c37a27c0af641096ce05bba192e3b505969a21c3de7f9d5c6058d03fe4ab5ee116d644f3e54f2f9e"
     + "d960ae613e23a07d5d7942ef046793745aeb9447712c8e86acc776b18ebb5fd63182a221f48823c563b8e4a3210f4911e305642a723"
--- UnitTests/UnitTests.CodeTiger.Core/AssemblyAttributes.cs
+++ UnitTests/UnitTests.CodeTiger.Core/AssemblyAttributes.cs
@@ -1,3 +1,5 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
+[assembly: CLSCompliant(false)]
 [assembly: CollectionBehavior(CollectionBehavior.CollectionPerClass, MaxParallelThreads = 8)]
