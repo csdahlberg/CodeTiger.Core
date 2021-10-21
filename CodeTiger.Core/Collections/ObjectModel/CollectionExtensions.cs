e6ea6e0ece89e01c5627a0e2fb06f9ef52dc29d8
Chris Dahlberg <csdahlberg@gmail.com>
2021-10-21 18:31:16 -0500

    Added an AddRange extension method for ICollection<T>.

---
+++ CodeTiger.Core/Collections/ObjectModel/CollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace CodeTiger.Collections.ObjectModel
+{
+    /// <summary>
+    /// Contains extension methods for the <see cref="ICollection{T}"/> class.
+    /// </summary>
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        /// Adds a collection of values to a <see cref="ICollection{T}"/> object.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the collection.</typeparam>
+        /// <param name="collection">The collection to add values to.</param>
+        /// <param name="values">The values to add to <paramref name="collection"/>.</param>
+        public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> values)
+        {
+            Guard.ArgumentIsNotNull(nameof(collection), collection);
+            Guard.ArgumentIsNotNull(nameof(values), values);
+            
+            foreach (var value in values)
+            {
+                collection.Add(value);
+            }
+        }
+    }
+}
---
+++ UnitTests/UnitTests.CodeTiger.Core/Collections/ObjectModel/CollectionExtensionsTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Moq;
+using Xunit;
+using CollectionExtensions = CodeTiger.Collections.ObjectModel.CollectionExtensions;
+
+namespace UnitTests.CodeTiger.Collections.ObjectModel
+{
+    public static class CollectionExtensionsTests
+    {
+        public class AddRange_ICollectionOfT_IEnumerableOfT
+        {
+            [Fact]
+            public void ThrowsArgumentNullExceptionWhenTargetCollectionIsNull()
+            {
+                Assert.Throws<ArgumentNullException>("collection",
+                    () => CollectionExtensions.AddRange(null!, Enumerable.Empty<object>()));
+            }
+
+            [Fact]
+            public void ThrowsArgumentNullExceptionWhenSourceCollectionIsNull()
+            {
+                ICollection<object> collection = new Collection<object>();
+
+                Assert.Throws<ArgumentNullException>("values",
+                    () => CollectionExtensions.AddRange(collection, null!));
+            }
+
+            [Fact]
+            public void DoesNotAddAnythingToTargetCollectionWhenSourceCollectionIsEmpty()
+            {
+                var collection = new Mock<ICollection<object>>(MockBehavior.Strict);
+
+                CollectionExtensions.AddRange(collection.Object, Enumerable.Empty<object>());
+
+                collection.Verify();
+            }
+
+            [Fact]
+            public void AddsSourceCollectionWithOneElementToTargetCollection()
+            {
+                var source = new List<Guid> { Guid.NewGuid() };
+
+                var collection = new Mock<ICollection<Guid>>(MockBehavior.Strict);
+                collection.Setup(x => x.Add(source[0]));
+
+                CollectionExtensions.AddRange(collection.Object, source);
+
+                collection.Verify();
+            }
+
+            [Fact]
+            public void AddsSourceCollectionWithMultipleElementsToTargetCollection()
+            {
+                var source = new List<Guid> { Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid() };
+
+                var collection = new Mock<ICollection<Guid>>(MockBehavior.Strict);
+                collection.Setup(x => x.Add(source[2]));
+                collection.Setup(x => x.Add(source[1]));
+                collection.Setup(x => x.Add(source[0]));
+
+                CollectionExtensions.AddRange(collection.Object, source);
+
+                collection.Verify();
+            }
+        }
+    }
+}
